78ebf2d4be54b6d1f76e5b691b086715fe254917
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2020-10-13 22:03:13 -0700

    added members and displayed members

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Form from "./Form";
 
-function App() {
+const membersList = [
+  {member: "Joseph"},
+  {member: "Hanes"},
+  {member: "Audrey"}
+];
+
+const initialFormValues = {
+  member: "",
+};
+
+export default function App() {
+  const [members, setMembers] = useState(membersList); // careful what you initialize your state to
+
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const updateForm = (inputName, inputValue) => {
+    setFormValues({
+      ...formValues,
+      [inputName]: inputValue,
+    });
+  };
+  const submitForm = () => {
+    let newMember = {
+      member: formValues.member,
+    };
+    setMembers([...members, newMember])
+    setFormValues(initialFormValues);
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Members</h1>
+      <Form
+        values={formValues}
+        update={updateForm}
+        submit={submitForm}
+      />
+      {members.map((item, idx) => {
+        return (
+          <div key={idx}>
+            {item.member}
+          </div>
+        )
+      })}
     </div>
   );
 }
-
-export default App;
